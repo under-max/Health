cb9b789cdad81f6b887c8072f78a04bf5f664c00
kookjin <aingdevil10@naver.com>
2023-05-22 21:45:47 +0900

    mybatis setting final

--- src/frontend/src/views/Login.vue
+++ src/frontend/src/views/Login.vue
@@ -1,6 +1,34 @@
 <template>
-  <h1>로그인!</h1>
+    <div class="login-container">
+        <form class="login-form">
+            <div class="form-group">
+                <label for="email">이메일</label>
+                <input id="email" type="email" v-model="email"/>
+            </div>
+            <div class="form-group">
+                <label for="password">비밀번호</label>
+                <input id="password" type="password" v-model="password"/>
+            </div>
+
+            <span class="el-button" style="color: #1a1e21">
+            <label for="keep">로그인정보저장 &nbsp;</label>
+            <input type="checkbox" id="keep" v-model="keepLogin" model-value=true/>
+            </span>
+            <button class="btn" @click.prevent="login()">로그인</button>
+            <div>
+                <RouterLink style="color:white" to="/signup">
+                    <button class="btn mt-2">
+                        회원가입
+                    </button>
+                </RouterLink>
+            </div>
+
+
+        </form>
+
+    </div>
 </template>
+
 
 <script>
 
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,4 +1,4 @@
 package com.example.demo.entity;
 
-public class Member {
+public class User {
 }
