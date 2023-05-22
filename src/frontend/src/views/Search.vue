8226062ff012f3f80ff510eefc9da87edc122629
under-max <saigrams@naver.com>
2023-05-22 16:58:01 +0900

    StartPackage final setting

--- src/frontend/src/views/Search.vue
+++ src/frontend/src/views/Search.vue
@@ -1,8 +1,23 @@
 <template>
   <h1>서치!</h1>
+  
+  <label for="company">회사명</label>
+  <input type="text" id="company"/><br>
+  
+  <label for="address">주소</label>
+  <input type="text" id="address"/><br>
+  
+  <label for="phoneNumber">전화번호</label>
+  <input type="text" id="phoneNumber"/><br>
+  
+  <button type="submit" @submit.prevent>보내기</button>
+  <form action=""></form>
+  
+
+
 </template>
 
-<script>
+<script setup>
 
 </script>
 
